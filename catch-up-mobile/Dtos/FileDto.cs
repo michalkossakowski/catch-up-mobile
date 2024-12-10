d03a3150fe2f3327b709e8b2fae91a15376a6997
ErykSliwowski <eryksliwowskic@gmail.com>
2024-12-10 17:48:15 +0100

    - added Dtos

---
+++ catch-up-mobile/Dtos/FileDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace catch_up_mobile.Dtos
+{
+    public class FileDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Type { get; set; }
+        public string? Source { get; set; }
+    }
+}
---
+++ catch-up-mobile/Dtos/MaterialDto.cs
@@ -0,0 +1,9 @@
+﻿namespace catch_up_mobile.Dtos
+{
+    public class MaterialDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public List<FileDto>? Files { get; set; }
+    }
+}

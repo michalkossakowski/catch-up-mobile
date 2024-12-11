fb6d2b97cb8e6189348fa57717c07ac12873ba09
ErykSliwowski <eryksliwowskic@gmail.com>
2024-12-12 00:40:19 +0100

    - showing files in material

---
+++ catch-up-mobile/Dtos/ApiResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace catch_up_mobile.Dtos
+{
+    public class ApiResponse<T>
+    {
+        public string Message { get; set; }
+        public T Data { get; set; }
+        
+    }
+
+}
--- catch-up-mobile/Dtos/MaterialDto.cs
+++ catch-up-mobile/Dtos/MaterialDto.cs
@@ -1,9 +1,12 @@
-﻿namespace catch_up_mobile.Dtos
+﻿using System.Text.Json.Serialization;
+
+namespace catch_up_mobile.Dtos
 {
     public class MaterialDto
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+
         public List<FileDto>? Files { get; set; }
     }
 }

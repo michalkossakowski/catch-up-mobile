ceb246921f2935a6b8cb6ae4aa8a02debc06ce3d
Triadziuch <kurowski1234567890@gmail.com>
2024-12-12 21:53:29 +0100

    Zaimplementowano listę TaskContent z możliwością dodawania, edycji i usuwania.

---
+++ catch-up-mobile/Dtos/MaterialDto.cs
@@ -0,0 +1,9 @@
+﻿namespace catch_up_backend.Dtos
+{
+    public class MaterialDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public List<FileDto>? Files { get; set; }
+    }
+}

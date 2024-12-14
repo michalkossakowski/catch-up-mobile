e0622767bb93851cc31633734dec4dcef04fc51e
ErykSliwowski <eryksliwowskic@gmail.com>
2024-12-14 03:49:31 +0100

    - implemented audio uploading to database

--- catch-up-mobile/Dtos/FileDto.cs
+++ catch-up-mobile/Dtos/FileDto.cs
@@ -1,11 +1,17 @@
 ﻿
+using System.Text.Json.Serialization;
+
 namespace catch_up_mobile.Dtos
 {
     public class FileDto
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+        [JsonPropertyName("name")]
         public string? Name { get; set; }
+        [JsonPropertyName("type")]
         public string? Type { get; set; }
+        [JsonPropertyName("source")]
         public string? Source { get; set; }
     }
 }

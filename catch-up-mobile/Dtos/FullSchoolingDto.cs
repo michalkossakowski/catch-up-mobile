ca2c9d92520d36db0d173da328928acf72c7e1b3
InterfectoremCubiculum <eryksliwowskic@gmail.com>
2024-12-15 20:18:26 +0100

    - added schooling list accordion

---
+++ catch-up-mobile/Dtos/FullSchoolingDto.cs
@@ -0,0 +1,16 @@
+﻿namespace catch_up_mobile.Dtos
+{
+    public class FullSchoolingDto
+    {
+        public SchoolingDto Schooling {get; set;}
+        public CategoryDto Category { get; set; }
+        public List<SchoolingPartDto> Parts { get; set; }
+
+        public FullSchoolingDto(SchoolingDto schooling, CategoryDto category, List<SchoolingPartDto> parts)
+        {
+            Schooling = schooling;
+            Category = category;
+            Parts = parts;
+        }
+    }
+}
---
+++ catch-up-mobile/Dtos/SchoolingPartDto.cs
@@ -0,0 +1,11 @@
+﻿namespace catch_up_mobile.Dtos
+{
+    public class SchoolingPartDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public List<MaterialDto> Materials { get; set; }
+
+    }
+}

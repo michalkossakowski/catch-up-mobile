ec6abfe9a8a033ba6a0151d82278de2c25185749
Werykowiak <88315629+Werykowiak@users.noreply.github.com>
2024-12-19 06:34:04 +0100

    Tasks form and basic Tasks view

---
+++ catch-up-mobile/Dtos/FullTask.cs
@@ -0,0 +1,27 @@
+﻿using catch_up_mobile.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace catch_up_mobile.Dtos
+{
+    internal class FullTask
+    {
+        public int Id { get; set; }
+        public Guid? NewbieId { get; set; }
+        public int? MaterialsId { get; set; }
+        public int? CategoryId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int? RoadMapPointId { get; set; }
+        public StatusEnum Status { get; set; }
+        public DateTime AssignmentDate { get; set; }
+        public DateTime? FinalizationDate { get; set; }
+        public DateTime? Deadline { get; set; }
+        public int SpendTime { get; set; }
+        public int Priority { get; set; }
+        public int? Rate { get; set; }
+    }
+}
---
+++ catch-up-mobile/Dtos/TaskDto.cs
@@ -0,0 +1,31 @@
+﻿using catch_up_mobile.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace catch_up_mobile.Dtos
+{
+    class TaskDto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public Guid? NewbieId { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        public int TaskContentId { get; set; }
+        public int? RoadMapPointId { get; set; }
+        public StatusEnum Status { get; set; }
+        public DateTime AssignmentDate { get; set; }
+        public DateTime? FinalizationDate { get; set; }
+        public DateTime? Deadline { get; set; }
+        public int SpendTime { get; set; }
+        public int Priority { get; set; }
+        public int? Rate { get; set; }
+
+
+        public TaskDto() { }
+        
+    }
+}

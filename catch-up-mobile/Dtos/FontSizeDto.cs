1ad4d62f3028cd5e817cfc05de168810cb671a7a
michalkossakowski <141955539+michalkossakowski@users.noreply.github.com>
2024-12-25 02:25:25 +0100

    #PT2024OBUE-182-added-select-font-size-MOBILE

---
+++ catch-up-mobile/Dtos/FontSizeDto.cs
@@ -0,0 +1,12 @@
+﻿
+using SQLite;
+
+namespace catch_up_mobile.Dtos
+{
+    public class FontSizeDto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string FontSize { get; set; }
+    }
+}

8b576f06462cb4fc68f9c767956e95dec60c6132
Triadziuch <kurowski1234567890@gmail.com>
2025-01-14 00:32:03 +0100

    Naprawiono offlline mode w TaskContent

--- catch-up-mobile/Dtos/CategoryDto.cs
+++ catch-up-mobile/Dtos/CategoryDto.cs
@@ -1,7 +1,10 @@
-﻿namespace catch_up_mobile.Dtos
+﻿using SQLite;
+
+namespace catch_up_mobile.Dtos
 {
     public class CategoryDto
     {
+        [PrimaryKey]
         public int Id { get; set; }
         public string? Name { get; set; }
     }

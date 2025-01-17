df01cb56d32d6d3aab2a7fcb5ee9ed65582a61f3
Mateusz Izdebski <mateusz.izdebski125@gmail.com>
2025-01-17 18:06:47 +0100

    dodanie obsługi zmiany opcji, dodanie settingsDTO

---
+++ catch-up-mobile/Dtos/CompanySettingsDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace catch_up_mobile.Dtos 
+{
+    public class CompanySettingsDto
+    {
+        public int Id { get; set; }
+        public Dictionary<string, bool> Settings { get; set; } = new Dictionary<string, bool>();
+    }
+}

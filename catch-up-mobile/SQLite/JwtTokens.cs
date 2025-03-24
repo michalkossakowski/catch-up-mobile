11299ee6b8b10b6e4f9a30d2e7caec6a3e854bd8
Kex <krzysieksobol9@gmail.com>
2025-03-24 14:30:39 +0100

    fixed the navbar and added a JwtTokens class

---
+++ catch-up-mobile/SQLite/JwtTokens.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+
+namespace catch_up_mobile.SQLite
+{
+    public static class JwtTokens
+    {
+        private static string _accessToken;
+        private static string _refreshToken;
+        private static DateTime _accessTokenExpiry;
+
+        public static string AccessToken
+        {
+            get => _accessToken;
+            set => _accessToken = value;
+        }
+
+        public static string RefreshToken
+        {
+            get => _refreshToken;
+            set => _refreshToken = value;
+        }
+
+        public static DateTime AccessTokenExpiry
+        {
+            get => _accessTokenExpiry;
+            set => _accessTokenExpiry = value;
+        }
+
+        public static void SetTokens(string accessToken, string refreshToken)
+        {
+            _accessToken = accessToken;
+            _refreshToken = refreshToken;
+            var handler = new JwtSecurityTokenHandler();
+            var jwtToken = handler.ReadJwtToken(accessToken);
+
+            var expClaim = jwtToken.Claims.FirstOrDefault(c => c.Type == "exp");
+            if (expClaim != null && long.TryParse(expClaim.Value, out long expSeconds))
+            {
+                _accessTokenExpiry = DateTimeOffset.FromUnixTimeSeconds(expSeconds).DateTime;
+            }
+            else
+            {
+                _accessTokenExpiry = DateTime.UtcNow.AddHours(1);
+            }
+        }
+
+        public static void ClearTokens()
+        {
+            _accessToken = null;
+            _refreshToken = null;
+            _accessTokenExpiry = DateTime.MinValue;
+        }
+
+        public static bool IsAccessTokenValid()
+        {
+            return !string.IsNullOrEmpty(_accessToken) && DateTime.UtcNow < _accessTokenExpiry;
+        }
+    }
+}
